fbe72adb9f5f8da29eec107f959f1a6ed976c15e
Jessica O'Brien <146675559+jnobrien@users.noreply.github.com>
2024-06-09 16:30:45 +0100

    add function for min days and date to complete quests

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-function daysLeft() {
-    const date = new Date();
-    const today = date.getDate();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
-    const daysLeft = daysInMonth(year, month) - today;
+const date = new Date();
+const today = date.getDate();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
+const daysLeft = daysInMonth(year, month) - today;
+
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+function daysLeftFunction() {
     document.getElementById('days-left').innerText = daysLeft;
 };
 
-daysLeft();
+daysLeftFunction();
 
-function questsLeft() {
+function questsLeftFunction() {
     const requiredQuests = document.getElementById('required-total').value;
     const questsDone = document.getElementById('current-total').value;
     const questsLeft = requiredQuests - questsDone;
     document.getElementById('quests-left').innerText = questsLeft;
 };
 
-function minQuests() {
-    const date = new Date();
-    const today = date.getDate();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
-    const daysLeft = daysInMonth(year, month) - today;
-
+function minQuestsFunction() {
     const requiredQuests = document.getElementById('required-total').value;
     const questsDone = document.getElementById('current-total').value;
     const questsLeft = requiredQuests - questsDone;
 
     const minQuests = questsLeft / daysLeft;
     document.getElementById('avg-quests').innerText = minQuests.toFixed(2);
+};
+
+function minDaysFunction() {
+    const requiredQuests = document.getElementById('required-total').value;
+    const questsDone = document.getElementById('current-total').value;
+    const questsLeft = requiredQuests - questsDone;
+
+    const minDays = Math.ceil(questsLeft / 6);
+    document.getElementById('min-days').innerText = minDays;
+
+    const completeDate = today + minDays;
+    const monthName = monthNames[month - 1];
+    document.getElementById('min-date').innerText = completeDate + " " + monthName + " " + year;
 }
 
 function calculate() {
-    questsLeft();
-    minQuests();
+    questsLeftFunction();
+    minQuestsFunction();
+    minDaysFunction();
 };
