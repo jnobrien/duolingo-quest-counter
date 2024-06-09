93d2f3335767971b2cfff12d0385f9a6724cfa16
Jessica O'Brien <146675559+jnobrien@users.noreply.github.com>
2024-06-09 15:56:39 +0100

    add function for avg quests per day

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ function questsLeft() {
     document.getElementById('quests-left').innerText = questsLeft;
 };
 
+function minQuests() {
+    const date = new Date();
+    const today = date.getDate();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
+    const daysLeft = daysInMonth(year, month) - today;
+
+    const requiredQuests = document.getElementById('required-total').value;
+    const questsDone = document.getElementById('current-total').value;
+    const questsLeft = requiredQuests - questsDone;
+
+    const minQuests = questsLeft / daysLeft;
+    document.getElementById('avg-quests').innerText = minQuests.toFixed(2);
+}
+
 function calculate() {
     questsLeft();
+    minQuests();
 };
